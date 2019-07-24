a6844dddb35b52511201ba41ed24c9eda489a9f7
Connor Duncan <ctdunc@berkeley.edu>
2019-07-24 12:33:07 -0700

    gettign ready to merge with local serv

--- sig.py
+++
@@ -1,4 +0,0 @@
-from command.daq_cmd import DAQCommander
-
-d = DAQCommander(redis_channels=["def"])
-d.configure(ni_task={"grep":"integrate","sususu":["www",10]}, socketio={"channel":"CCCCC"}, savefile={"jibberscribs":"jesus"})
