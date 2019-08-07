6d46d5c3ebf41ea7b2c4eaff10a083d9bedee362
Connor Duncan <connor.t.duncan@gmail.com>
2019-08-06 17:34:31 -0700

    update cworker

--- cworker.py
+++ cworker.py
@@ -5,14 +5,14 @@ test_listener = TestListener(
         "test", 
         {
         "cfg_analog_input":{
-        "channels": ["Dev1/ai0", "Dev1/ai1", "Dev1/ai2"],
+            "channels": {"Dev1/ai0":"off", "Dev1/ai1":"off", "Dev1/ai2":"off"},
             "max_sample_rate": 100000,
             "min_sample_rate": 100,
             "sr_is_per_chan": False,
             "trigger_opts": []
             },
         "cfg_digital_input":{
-        "channels": ["Dev1/di0", "Dev1/di1", "Dev1/di2"],
+            "channels": {"Dev1/di0":"off", "Dev1/di1":"off", "Dev1/di2":"off"},
             "max_sample_rate": 100000,
             "min_sample_rate": 100,
             "sr_is_per_chan": False,
