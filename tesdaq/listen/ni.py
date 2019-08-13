de98733614df83c8f367f54f952a9348fbb58963
ctdunc <connor.t.duncan@gmail.com>
2019-08-13 11:10:50 -0700

    added device guess to ni class. to test soon

--- tesdaq/listen/ni.py
+++ tesdaq/listen/ni.py
@@ -70,12 +70,11 @@ def task_restrict_for_device(devicename):
     # TODO: fix return type
     device = nidaqmx.system.device.Device(devicename)
     # get analog input info
-    ai_restriction, ai_backend = ai_task_restrict(device)
+    ai_restriction = ai_task_restrict(device)
     # get digital input info
-    di_restriction, di_backend = di_task_restrict(device)
+    di_restriction = di_task_restrict(device)
     device_restrictions = {"analog_input": ai_restriction, "digital_input": di_restriction}
-    backend_maps = {"analog_input": ai_backend, "digital_input": di_restriction}
-    return device_restrictions, backend_maps
+    return device_restrictions
 def di_task_restrict(device):
     """di_task_restrict
     Attempts to automatically generate a TaskTypeRestriction for a given NI devices digital input channels. 
@@ -89,9 +88,6 @@ def di_task_restrict(device):
     -------
     task_restrict: tesdaq.listen.parameters.TaskTypeRestriction
         TaskTypeRestriction containing constraints on task
-    backend_maps: dict
-        Dict containing key, value pairs that map between named sample modes and their associated enum.
-        Each key is the named argument of the TaskTypeRestriction, with each value containing the actual enum type.
     """
     chans = device.di_lines.channel_names
     trig_usage = __map_enum_to_dict(device.di_trig_usage)
@@ -102,15 +98,14 @@ def di_task_restrict(device):
             num_tasks=1,
             valid_channels=chans,
             valid_timing=[], # TODO: fix timing
-            valid_trigger=trig_usage.keys(),
+            valid_trigger=trig_usage,
             max_sample_rate=max_rate,
             min_sample_rate=min_rate,
             volt_ranges=[()], # TODO: fix volt range to be 0, logic_on
             sr_is_per_chan=True
             )
-    backend_maps = {"trig_usage": trig_usage}
 
-    return task_restrict, backend_maps
+    return task_restrict
 def ai_task_restrict(device):
     """ai_task_restrict
     Attempts to automatically generate a TaskTypeRestriction for a given NI devices analog input channels. 
@@ -124,13 +119,6 @@ def ai_task_restrict(device):
     -------
     task_restrict: tesdaq.listen.parameters.TaskTypeRestriction
         TaskTypeRestriction containing constraints on task
-    backend_maps: dict
-        Dict containing key, value pairs that map between named sample modes and their associated enum.
-        Each key is the named argument of the TaskTypeRestriction, with each value containing the actual enum type.
-
-    Notes
-    -----
-    backend_maps exists to associate named frontend arguments (strings such as CONTINUOUS) to their nidaqmx enum values.
     """
     chans = device.ai_physical_chans.channel_names
     samp_modes = __map_enum_to_dict(device.ai_samp_modes)
@@ -146,23 +134,51 @@ def ai_task_restrict(device):
             num_tasks=1, #TODO: Add support for more tasks
             valid_channels=chans,
             valid_timing=samp_modes,
-            valid_trig_usage=trig_usage.keys(),
+            valid_trig_usage=trig_usage,
             max_sample_rate=max_sample,
             min_sample_rate=min_sample,
             volt_ranges=volt_ranges,
             sr_is_per_chan=False #TODO: Add support for more tasks
             )
-    backend_maps = {"valid_timing" : samp_modes, "valid_trigger": trig_usage}
 
-    return task_restrict, backend_maps
+    return task_restrict 
     
 class DAQmxListener(DeviceListener):
     def __init__(self,
             identifier,
             redis_instance,
+            device_name,
             **kwargs):
+        # TODO: add support for multiple devices
+        """__init__
+
+        Parameters
+        ----------
+        identifier: str
+            Unique string used to get and set values in redis database.
+        redis_instance: redis.Redis
+            Redis instance to connect
+        device_name: str
+            nidaqmx name of physical device (e.g. "Dev1").
+        Keyword Args
+        ------------
+        analog_input: (tesdaq.listen.parameters.TaskTypeRestriction, dict of nidaqmx Enum)
+            TaskTypeRestriction on 'analog_input' tasks. Corresponds to nidaqmx cfg_analog_input restrictions.
+            If none, will attempt to autogenerate restrictions from current device settings. The nidaqmx enums should be named according to their functions in the TaskTypeRestriction.
+            There will be an example of this soon.
+        digital_input: tesdaq.listen.parameters.TaskTypeRestriction
+            TaskTypeRestriction on 'digital_input' tasks.
+            If none, will attempt to autogenerate restrictions from current device settings.
+        """
+        # TODO: Write example of passing dict of nidaqmx enum with kwarg
+        if not kwargs:
+            # If restriction passed as none, generate restriction
+            device_restrictions = task_restrict_for_device(devicename)
+        else:
+            # Restriction = explicitly passed TaskTypeRestriction
+            device_restrictions = kwargs 
         super(DAQmxListener, self).__init__(identifier, redis_instance, **kwargs)
-        print(self.restrictions) 
+        print(device_restrictions, self.restrictions)
     def configure(self, **kwargs):
         print(self.state) 
     def start(self):
